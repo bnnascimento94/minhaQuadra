19564b17c3c428f66f7aba2dd1b009ba15a79b9f
Perkles <brunon@itmchannelmarketing.com>
2022-03-21 09:31:48 -0300

    ajustments dagger hilt and development of screen ForgotPasswordFragment.kt

---
+++ app/src/main/java/com/example/minhaquadra/data/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.minhaquadra.data.model
+
+data class User(
+    val uid: String?,
+    val name: String?,
+    val email: String?,
+    val photoUrl: String?,
+    val userIsEmailVerified:Boolean?
+)
